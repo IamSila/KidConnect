4ca9d4214c2c6c4fdae21053f90db51b91b40809
IamSila <silamulingi@gmail.com>
2024-12-01 16:51:29 +0300

    all created thing and all updates pushed

--- mainApp/models.py
+++ mainApp/models.py
@@ -7,7 +7,24 @@ from django.db import models
 
 
 """teams model"""
+"""Teams Model"""
+class Team(models.Model):
+    """creating a model to store the details for the teams sections
+    - Full name
+    - profile photo
+    - position
+    - description
+    [these will be filled in by the admin for now or we can create a page for that]
+    """
 
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    description = models.TextField()
+    profile_photo = models.ImageField(upload_to='profiles')
+
+    def __str__(self):
+        """returns the full_name of the object as the name of that object"""
+        return f"{self.full_name} ----> {self.position}"
 
 
 """portfolio model"""
@@ -16,13 +33,13 @@ from django.db import models
 
 """analytics model"""
 class Analytic(models.Model):
-    client = models.IntegerField()
-    projects = models.IntegerField()
-    hoursOfSupport = models.IntegerField()
+    reportedChildren = models.IntegerField()
+    foundChildren = models.IntegerField()
+    missingChildren = models.IntegerField()
     workers = models.IntegerField()
 
 
-    # """defining a name for each entry"""
-    # def __str__(self):
-    #     return "analytics"
+    """defining a name for each entry"""
+    def __str__(self):
+        return f"analytics {self.id}"
 
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from . models import Analytic
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {}
+    Analytics = Analytic.objects.all()
+    context['analytics'] = Analytics
+    
+    return render(request, 'index.html', context)
